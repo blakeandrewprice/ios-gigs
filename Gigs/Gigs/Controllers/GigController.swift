8f34a6f338931e36b4cca39db94b870154549765
Blake Price <blakeandrewprice@gmail.com>
2019-11-07 14:48:50 -0800

    On Part 2, added signin, signup, segmented control, button tap methods and constraints

--- Gigs/Gigs/Controllers/GigController.swift
+++ Gigs/Gigs/Controllers/GigController.swift
@@ -7,8 +7,100 @@
 //
 
 import Foundation
+import UIKit
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+enum NetworkError: Error {
+    case noAuth
+    case badAuth
+    case otherError
+    case badData
+    case noDecode
+}
 
 class GigController {
     var bearer: Bearer?
-    let baseURL = URL(string: "https://lambdagigs.vapor.cloud/api")
+    private let baseUrl = URL(string: "https://lambdagigs.vapor.cloud/api")!
+    
+    func signUp(with user: User, completion: @escaping (Error?) -> ()) {
+        let signUpUrl = baseUrl.appendingPathComponent("users/signup")
+        
+        var request = URLRequest(url: signUpUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        URLSession.shared.dataTask(with: request) { (_, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo:nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
+    }
+    
+    func signIn(with user: User, completion: @escaping (Error?) -> ()) {
+        let loginUrl = baseUrl.appendingPathComponent("users/login")
+        
+        var request = URLRequest(url: loginUrl)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            print("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(NSError(domain: "", code: response.statusCode, userInfo:nil))
+                return
+            }
+            
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                self.bearer = try decoder.decode(Bearer.self, from: data)
+            } catch {
+                print("Error decoding bearer object: \(error)")
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
+    }
 }
--- Gigs/Gigs/Controllers/LoginViewController.swift
+++ Gigs/Gigs/Controllers/LoginViewController.swift
@@ -8,17 +8,77 @@
 
 import UIKit
 
+enum LoginType {
+    case signUp
+    case signIn
+}
+
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var signUpInButton: UIButton!
+    @IBOutlet weak var signInButton: UIButton!
     
-    @IBAction func switchSegmentedControl(_ sender: Any) {
+    var gigController: GigController!
+    var loginType = LoginType.signUp
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        signInButton.backgroundColor = UIColor(hue: 190/360, saturation: 70/100, brightness: 80/100, alpha: 1.0)
+        signInButton.tintColor = .white
+        signInButton.layer.cornerRadius = 8.0
     }
     
+    @IBAction func signInTypeChanged(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            loginType = .signUp
+            signInButton.setTitle("Sign Up", for: .normal)
+        } else {
+            loginType = .signIn
+            signInButton.setTitle("Sign In", for: .normal)
+        }
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        guard let gigController = gigController else { return }
+        if let username = usernameTextField.text,
+            !username.isEmpty,
+            let password = passwordTextField.text,
+            !password.isEmpty {
+            let user = User(username: username, password: password)
+            
+            if loginType == .signUp {
+                gigController.signUp(with: user) { error in
+                    if let error = error {
+                        print("Error occurred during sign up: \(error)")
+                    } else {
+                        DispatchQueue.main.async {
+                            let alertController = UIAlertController(title: "Sign Up Successful", message: "Now please log in.", preferredStyle: .alert)
+                            let alertAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                            alertController.addAction(alertAction)
+                            self.present(alertController, animated: true, completion: {
+                                self.loginType = .signIn
+                                self.segmentedControl.selectedSegmentIndex = 1
+                                self.signInButton.setTitle("Sign In", for: .normal)
+                            })
+                        }
+                    }
+                }
+            } else {
+                gigController.signIn(with: user) { error in
+                    if let error = error {
+                        print("Error occurred during sign up: \(error)")
+                    } else {
+                        DispatchQueue.main.async {
+                            self.dismiss(animated: true, completion: nil)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
